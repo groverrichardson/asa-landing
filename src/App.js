d0a7a6ec2ad46f618dfc646fd9cd062906427ac9
Grover Richardson <groverrichardson1987@gmail.com>
2021-11-03 16:10:15 -0700

    Project initialized

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Test from './components/test';
+import 'tailwindcss/tailwind.css';
+
+function App() {
+    return (
+        <main className="App">
+            <Test />
+        </main>
+    );
+}
+
+export default App;
