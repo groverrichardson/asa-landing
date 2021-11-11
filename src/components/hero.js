87aaab19c3f1bc59e747a6c3a9104303edd7058c
Grover Richardson <groverrichardson1987@gmail.com>
2021-11-11 15:37:59 -0800

    Fixed padding issue in hero copy section.

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,7 +5,7 @@ import HeroImg from '../assets/hero.jpeg';
 
 function Hero() {
     return (
-        <section className="hero-section-container bg-cover flex flex-row flex-wrap relative mobileS:mb-48 iPadPro:mb-0 w-full iPadPro:overflow-x-hidden">
+        <section className="hero-section-container bg-cover flex flex-row flex-wrap relative mobileS:mb-48 iPadPro:mb-0 w-full iPadPro:overflow-hidden">
             <header className="flex items-center justify-between px-5 py-2 w-full self-start">
                 <img
                     src={Logo}
@@ -15,7 +15,7 @@ function Hero() {
                 <Cta visibility="invisible laptop:visible" />
             </header>
             <section className="hero-container flex flex-row py-0 items-center justify-center flex-wrap iPad:flex-1 iPadPro:flex-nowrap iPadPro:h-96">
-                <div className="hero-copy-container w-full px-12 pt-6 pb-12 lg:p-24 relative overflow-x-hidden flex flex-col justify-center items-center iPadPro:px-0 iPadPro:px-12 iPadPro:static">
+                <div className="hero-copy-container w-full px-12 pt-6 pb-12 relative overflow-x-hidden flex flex-col justify-center items-center iPadPro:px-0 iPadPro:px-12 iPadPro:static">
                     <div className="hero-header container max-w-xl">
                         <h1 className="hero-header whitespace-pre-wrap text-4xl iPhoneX:text-5xl iPad:text-4xl text-primary font-work tracking-tighter font-bold ">
                             Quality you can trust,{' '}
